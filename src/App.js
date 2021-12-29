bff87fda93ed822f1ac8e008645972295bc01c22
Dheerendra Kumar <53529978+have01@users.noreply.github.com>
2021-12-29 11:31:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { useState } from 'react';
+
+
+
+function App() {
+  const [name, setName] = useState('')
+  const [fullname,setfullname] = useState();
+const[bg,setbg]=useState('aliceblue')
+ 
+  const val =(event)=>{
+    setName(event.target.value);
+    event.preventDefault();
+    setbg('aliceblue')
+    
+  
+}
+
+const hello= ()=>{
+  setfullname(name);
+  setbg('yellow')
+}
+  return (
+    <div className="App" style={{backgroundColor: bg}}>
+    <h1>Reacts Hooks</h1> <br />
+    <h1>Hello {fullname}</h1>
+    <input placeholder='Enter your Name' type="text"  onChange={val}  value={name}  />
+    <br />
+    <button onClick={hello}> Submit</button>
+    </div>
+  );
+}
+
+export default App;
